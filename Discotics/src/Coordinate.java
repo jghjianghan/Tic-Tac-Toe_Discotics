99a3df32e8b9eb164ec4ad3616b5f0dbf506118a
Jiang Han <jianghan99@gmail.com>
2019-11-22 12:42:38 +0700

    Updating Cooordinate and State1
    Adding utility method in Coordinate for integer-Coordinate conversion.
    Adding state S0 to State1.

--- Discotics/src/Coordinate.java
+++ Discotics/src/Coordinate.java
@@ -25,4 +25,60 @@ public class Coordinate {
         return this.kolom;
     }
     
+    public static int coordinateToNumber(Coordinate n) {
+        switch (n.getBaris()) {
+            case 0:
+                switch (n.getKolom()) {
+                    case 0:
+                        return 1;
+                    case 1:
+                        return 2;
+                    case 2:
+                        return 3;
+                }
+            case 1:
+                switch (n.getKolom()) {
+                    case 0:
+                        return 4;
+                    case 1:
+                        return 5;
+                    case 2:
+                        return 6;
+                }
+            case 2:
+                switch (n.getKolom()) {
+                    case 0:
+                        return 7;
+                    case 1:
+                        return 8;
+                    case 2:
+                        return 9;
+                }
+        }
+        return 0;
+    }
+    
+    public static Coordinate numberToCoordinate(int n) {
+        switch (n) {
+            case 1:
+                return new Coordinate(0, 0);
+            case 2:
+                return new Coordinate(0, 1);
+            case 3:
+                return new Coordinate(0, 2);
+            case 4:
+                return new Coordinate(1, 0);
+            case 5:
+                return new Coordinate(1, 1);
+            case 6:
+                return new Coordinate(1, 2);
+            case 7:
+                return new Coordinate(2, 0);
+            case 8:
+                return new Coordinate(2, 1);
+            case 9:
+                return new Coordinate(2, 2);
+        }
+        return null;
+    }
 }
